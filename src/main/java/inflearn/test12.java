2186496831486e520f4641d1cb0f72504d8131d2
ssosee <ssosee@gmail.com>
2022-06-15 02:09:29 +0900

    인프런 1. 8문항 복습 2. 12문항 실습

---
+++ src/main/java/inflearn/test12.java
@@ -0,0 +1,62 @@
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * <h1>암호</h1>
+ */
+public class test12 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        //System.out.println(new Solution().decryption(n, str));
+        System.out.println(new Solution().solution2(n, str));
+    }
+
+    public static class Solution {
+        /**
+         * #: 1
+         * *: 0
+         */
+        public String decryption(int n, String str) {
+            String answer = "";
+            String s = "";
+            char ch;
+
+            for(char c : str.toCharArray()) {
+                if(c == '*') s += '0';
+                else if (c == '#') s += '1';
+            }
+
+            for(int i = 0; i < n; i++) {
+                String strBin = s.substring(7*i, 7*(i+1));
+                ch = (char) Integer.parseInt(strBin, 2);
+                answer += ch;
+            }
+
+            return answer;
+        }
+
+        /**
+         * 강의 풀이
+         */
+        public String solution2(int n, String str) {
+            String answer = "";
+
+            for(int i = 0; i < n; i++) {
+                String tmp = str.substring(0, 7)
+                        .replace('#', '1')
+                        .replace('*', '0');
+                int num = Integer.parseInt(tmp, 2);
+                answer += (char) num;
+                str = str.substring(7); //시작 위치를 바꿈
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ src/main/java/inflearnReview/test8.java
@@ -0,0 +1,29 @@
+package inflearnReview;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test8 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(new Solution().getString(str));
+    }
+
+    public static class Solution {
+        public String getString(String str) {
+            String answer = "NO";
+
+            str = str.toUpperCase();
+            String s = "";
+            for(char c : str.toCharArray()) {
+                if(Character.isAlphabetic(c)) s += c;
+            }
+
+            if(s.equals(new StringBuilder(s).reverse().toString())) return "YES";
+
+            return answer;
+        }
+    }
+}
