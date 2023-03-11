05d5792841b3d127b3b495100610f9a3f91ddb0e
howisitgoing <howisitgoing@kakao.com>
2023-03-12 01:28:14 +0900

    인프런 스택과 큐

--- src/main/java/backjoon/datastructure/stack_queue/Quiz11286.java
+++ src/main/java/backjoon/datastructure/stack_queue/Quiz11286.java
@@ -17,41 +17,12 @@ public class Quiz11286 {
         }
 
         /**
-         * x가 0이 아닐 경우
-         *  배열에 값 추가
-         * x가 0인 경우
-         *  만약 배열이 비어있는 경우 0을 출력
-         *  배열에서 절댓값이 가장 작은 값 출력하고(여러개면 가장 작은 수 출력) 배열에서 제거
-         *
-         *  우선순위 큐를 이용함
-         *  우선순위 큐는 기본적으로 min heap
-         *
-         *  기본 값
-         *  o1 < o2 : -1
-         *  o1 == o2 : 0
-         *  o1 > o2  : 1
-         *  [o1, o2, o3, o4 , ...]
+         * 1. 절대값이 같으면 가장 작은수 부터 출력(입력값 기준 오름차순)
+         * 2. 절댓값이 가장 작은 값부터 출력(절댓값 기준 오름차순)
          */
-        // 우선순위 큐 정렬설정 변경
-        // 절대값이 같으면 오름차순 정렬(1, -1) -> [1, -1]
-        // 아니면 절대값으로 오름차순 정렬(-2, -4) -> [4, 2]
-        Queue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
-            /**
-             * 1. 절댓값이 가장 작은 값부터 출력
-             * 2. 절대값이 같으면 가장 작은수 부터 출력
-             */
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                // 절대값이 같으면 그중에서 작은 수로 오름차순
-                if(Math.abs(o1) == Math.abs(o2)) {
-                    return Integer.compare(o1, o2);
-                }
-                // 아니면 절대값이 작은 수로 오름차순
-                else {
-                    return Integer.compare(Math.abs(o1), Math.abs(o2));
-                }
-            }
-        });
+        Queue<Integer> queue = new PriorityQueue<>(
+                (o1, o2) -> Math.abs(o1) == Math.abs(o2) ? Integer.compare(o1, o2) : Integer.compare(Math.abs(o1), Math.abs(o2))
+        );
 
         StringBuilder sb = new StringBuilder();
         for(Integer v : x) {
---
+++ src/test/java/ComparatorTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class ComparatorTest {
+
+    @Test
+    void 오름차순_테스트() {
+        int[] A = {1, 2, 10, -5, 0, 8};
+        Queue<Integer> queue = new PriorityQueue<>();
+
+        for(int i : A) {
+            queue.add(i);
+        }
+
+        System.out.println(queue);
+
+        for(int i = 0; i < A.length; i++) {
+            System.out.print(queue.poll()+" ");
+        }
+    }
+
+    @Test
+    void 내림차순_테스트() {
+        int[] A = {1, 2, 10, -5, 0, 8};
+        Queue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(o1 > o2) {
+                    return -1;
+                } else if(o1 < o2) {
+                    return 1;
+                }
+                return 0;
+            }
+        });
+
+        for(int i : A) {
+            queue.add(i);
+        }
+
+        System.out.println(queue);
+
+        for(int i = 0; i < A.length; i++) {
+            System.out.print(queue.poll()+" ");
+        }
+    }
+}
