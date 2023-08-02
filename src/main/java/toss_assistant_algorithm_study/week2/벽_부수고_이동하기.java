f40b31bfa72850eb2684dacd43b6d04a210a1385
howisitgoing <howisitgoing@kakao.com>
2023-08-03 00:15:22 +0900

    토스 동료들과 알고리즘 스터디 - 벽 부수고 이동하기

--- src/main/java/toss_assistant_algorithm_study/week2/벽_부수고_이동하기.java
+++ src/main/java/toss_assistant_algorithm_study/week2/벽_부수고_이동하기.java
@@ -6,14 +6,22 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class 벽_부수고_이동하기 {
-
     /**
      * 1. 아이디어
-     *  dfs를 이용하여 x벽을 1개씩 부셨을 때의 최단거리를 모두 계산한다.
+     *  bfs를 이용하여 [x][y]벽을 1개씩 부셨을 때의 최단거리를 모두 계산한다.
+     *  vistied는 3차원 배열을 사용한다.
+     *      벽을 안부수고 탐색 [x][y][0],
+     *      벽을 부수고 탐색 [x][y][1]
+     *
+     *  벽을 부신적이 없으면 벽을 부신다.
+     *  벽을 부신적이 있으면 벽을 못 부신다.
      *
      * 2. 시간복잡도
+     *  O(M * N) -> O(1000 * 1000)
      *
      * 3. 자료구조
+     * Queue    -> new ArrayDeque<>()
+     * 3차원 배열 -> static boolean[][][] visited
      */
 
     static int n;
@@ -21,9 +29,7 @@ public class 벽_부수고_이동하기 {
     static int[] moveX = {1, -1, 0 ,0};
     static int[] moveY = {0, 0, 1 ,-1};
     static int[][] map;
-    static boolean[][] visited;
-    static int min = Integer.MAX_VALUE;
-    static List<Integer> results = new ArrayList<>();
+    static boolean[][][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -31,83 +37,61 @@ public class 벽_부수고_이동하기 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        map = new int[n+1][m+1];
-        for(int i = 1; i <= n; i++) {
+        map = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             String str = st.nextToken();
-            for(int j = 1; j <= m; j++) {
-                map[i][j] = str.charAt(j-1) - '0';
+            for (int j = 1; j <= m; j++) {
+                map[i][j] = str.charAt(j - 1) - '0';
             }
         }
 
-        // 벽을 안부순 경우
         int bfs = bfs();
-        // 벽 1개 부순경우
-        dfs(0);
+        System.out.print(bfs);
 
-        results.add(bfs);
-        for(Integer i : results) {
-            if(i != -1) {
-                min = Math.min(i, min);
-            }
-        }
-
-        if(min == Integer.MAX_VALUE) {
-            System.out.print(-1);
-            br.close();
-            return;
-        }
-
-        System.out.print(min);
         br.close();
     }
 
-    private static void dfs(int wall) {
-
-        // 벽 1개 부셨을 경우
-        if(wall == 1) {
-            results.add(bfs());
-            return;
-        }
-
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
-                if(map[i][j] == 1) {
-                    map[i][j] = 0; // 벽뚫기
-                    dfs(wall + 1);
-                    map[i][j] = 1; // 벽뚫은거 원복
-                }
-            }
-        }
-    }
-
     private static int bfs() {
-        visited = new boolean[n+1][m+1];
-        visited[1][1] = true;
+        visited = new boolean[n+1][m+1][2];
+        visited[1][1][0] = true;
+        visited[1][1][1] = true;
 
-        Queue<Node> queue = new ArrayDeque<>();
-        queue.offer(new Node(1, 1, 1));
+        Queue<Path> queue = new ArrayDeque<>();
+        queue.offer(new Path(1, 1, 1, false));
 
         while (!queue.isEmpty()) {
-            Node poll = queue.poll();
+            Path poll = queue.poll();
             int x = poll.x;
             int y = poll.y;
             int count = poll.count;
+            boolean isDestroyWall = poll.isDestroyWall;
 
-            if(x == n && y == m) {
-                return count;
-            }
+            if(x == n && y == m) return count;
 
-            for(int i = 0 ; i < 4; i++) {
+            for(int i = 0; i < 4; i++) {
                 int newX = x + moveX[i];
                 int newY = y + moveY[i];
 
-                // 범위체크
+                // 범위 체크
                 if(newX < 1 || newY < 1 || newX > n || newY > m) continue;
 
-                if(!visited[newX][newY] && map[newX][newY] == 0) {
-                    visited[newX][newY] = true;
-                    queue.offer(new Node(newX, newY, count + 1));
+                // 벽이 아니면
+                if(map[newX][newY] == 0) {
+                    // 이미 벽을 부순 경우
+                    if(isDestroyWall && !visited[newX][newY][1]) {
+                        visited[newX][newY][1] = true;
+                        queue.offer(new Path(newX, newY, count + 1, true));
+                    }
+                    // 벽을 부순적이 없는 경우
+                    else if(!isDestroyWall && !visited[newX][newY][0]) {
+                        visited[newX][newY][0] = true;
+                        queue.offer(new Path(newX, newY, count + 1, false));
+                    }
+                }
+                //벽이고 벽을 부순적이 없는 경우
+                else if(!isDestroyWall) {
+                    queue.offer(new Path(newX, newY, count + 1, true));
                 }
             }
         }
@@ -115,15 +99,17 @@ public class 벽_부수고_이동하기 {
         return -1;
     }
 
-    static class Node {
+    static class Path {
         private int x;
         private int y;
         private int count;
+        private boolean isDestroyWall;
 
-        public Node(int x, int y, int count) {
+        public Path(int x, int y, int count, boolean isDestroyWall) {
             this.x = x;
             this.y = y;
             this.count = count;
+            this.isDestroyWall = isDestroyWall;
         }
     }
 }
